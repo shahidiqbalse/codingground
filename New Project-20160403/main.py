1f3955952e58d8e1399917093a397e1706204b6e
Muhammad Shahid <shahidiqbal_se@hotmail.com>
2016-04-03 16:35:56 +0500

    Created by Coding Ground Client

---
+++ New Project-20160403/main.py
@@ -0,0 +1,5 @@
+
+import urllib
+import json
+response = urllib.urlopen("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22BHP.AX%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
+print json.load( response)
