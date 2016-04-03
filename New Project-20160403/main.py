47d59816565350ca478f98b83a5dc5ca4a1cac8a
Muhammad Shahid <shahidiqbal_se@hotmail.com>
2016-04-03 17:13:43 +0500

    Update main.py

--- New Project-20160403/main.py
+++ New Project-20160403/main.py
@@ -1,5 +1,9 @@
-
 import urllib
 import json
-response = urllib.urlopen("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22BHP.AX%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
-print json.load( response)
+response = urllib.urlopen("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=")
+#response = urllib.urlopen("http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo")
+json1 = json.load( response)
+print json1
+print type(json1)
+#print response["geonames"]
+print json1.keys()
